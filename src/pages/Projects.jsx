63c2140105caeaed7e9de3b7206b28d3018d29e5
RianAsuncion <jrafasuncion@addu.edu.ph>
2022-09-23 20:33:24 +0800

    firt commit

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Header, Button } from '../components';
+
+const Projects = () => (
+  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Projects" />
+    <Button
+      color="#334155"
+      text="+ Add Project"
+      borderRadius="10px"
+      cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+    />
+  </div>
+);
+
+export default Projects;
---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Kanban } from './Kanban';
+export { default as Employees } from './Employees';
+export { default as Calendar } from './Calendar';
+export { default as Projects } from './Projects';
